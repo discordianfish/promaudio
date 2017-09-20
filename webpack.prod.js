cc4046f436420cd2272313d3ed89ca072c7bb133
Johannes 'fish' Ziemke <github@freigeist.org>
2017-09-20 20:37:04 +0200

    Add publicPath prefix

---
+++ webpack.prod.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const Merge = require('webpack-merge');
+
+const CommonConfig = require('./webpack.config.js');
+
+module.exports = Merge(CommonConfig, {
+  output: {
+    publicPath: '/promaudio/'
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    // Appears to be needed even though we set NODE_ENV outside which again is
+    // needed for babel.
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+  ],
+});
