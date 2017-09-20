156e94ab75de4efea722c1fcde2d44cd77604a79
Johannes 'fish' Ziemke <github@freigeist.org>
2017-09-20 19:23:07 +0200

    Make tunable n stuff

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import { h, render, Component } from 'preact';
-
 import Player from './player';
+import 'purecss';
+import './css/style.css';
 
 class App extends Component {
   constructor() {
--- app/player.js
+++ app/player.js
@@ -1,19 +1,26 @@
 import { h, Component } from 'preact';
+import linkState from 'linkstate';
 
-const url = "http://localhost:42401";
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
+const defaultURL = "http://localhost:9090";
 const sampleRate = 44100;
+
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 export default class Player extends Component {
   constructor() {
     super();
     this.state = {
-      query: 'avg by (instance) (probe_http_duration_seconds)',
+      url: defaultURL,
+      query: 'go_goroutines',
       frame: 0,
+      end: Date.now()/1000,
+      range: 3600,
+      sampleCount: 1000,
+      sampleDuration: 0.1,
     }
     this.submit = this.submit.bind(this);
     this.tune = this.tune.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
   tune() {
@@ -21,7 +28,7 @@ export default class Player extends Component {
     this.state.samples.map((series) => {
       i++
       if (this.state.frame >= series.values.length) {
-        this.clock = null;
+        clearTimeout(this.clock);
         this.state.oscs[i].stop()
         return
       }
@@ -32,6 +39,12 @@ export default class Player extends Component {
 
       this.state.oscs[i].frequency.value = freq - (sampleRate/2)
     });
+  }
+  stop(e) {
+    e.preventDefault();
+    clearTimeout(this.clock);
+    this.state.oscs.map((osc) => { osc.stop() });
+    console.log("stopped");
   }
   submit(e) {
     e.preventDefault();
@@ -62,20 +75,19 @@ export default class Player extends Component {
         this.clock = setInterval(() => {
           this.setState({frame: this.state.frame+1});
           this.tune();
-        }, 100)
+        }, this.state.sampleDuration*1000)
       })
       .catch((err) => this.setState({error: err}));
   }
   query(query) {
-    const now = Date.now() / 1000
     const params = new URLSearchParams(Object.entries({
       query: query,
-      start: now - 3600,
-      end: now,
-      step: 30,
+      start: this.state.end - this.state.range,
+      end: this.state.end,
+      step: this.state.range / this.state.sampleCount,
     }));
 
-  	return fetch(url + '/api/v1/query_range?' + params.toString(), {
+  	return fetch(this.state.url + '/api/v1/query_range?' + params.toString(), {
       method: 'GET',
     })
     .then((resp) => {
@@ -90,19 +102,44 @@ export default class Player extends Component {
   }
 
   render(props, state) {
-    let error;
+    let results = "No results";
     if (this.state.error) {
-      error = <div className="error">{this.state.error.message}</div>
+      results = <div className="error">{this.state.error.message}</div>
+    } else {
+      if (this.state.samples && this.state.samples.length > 0) {
+        results = <pre>{JSON.stringify(this.state.samples, null, 2)}</pre>
+      }
     }
     return <div>
-      {error}
-      <form onSubmit={this.submit}>
-        <label>
-          <input type="text" id="query" name="query" value={this.state.query} onChange={
-            (e) => { this.setState({query: e.target.value})} }/>
-          Query
-        </label>
-        <button type="submit">Submit</button>
+      <form onSubmit={this.submit} className="pure-form">
+        <div className="pure-g">
+          <div className="pure-u-3-5">
+            <textarea style={{width: '100%', height: '10em'}} className="pure-input-2-3" id="query" name="query" value={this.state.query} onInput={linkState(this, 'query')}/>
+            <div>Play frame #{this.state.frame} / {this.state.sampleCount}</div>
+            <div className="results">{results}</div>
+          </div>
+          <div className="pure-u-2-5">
+            <fieldset>
+              <button style={{padding: '1em'}} className="pure-button" type="submit">&#9654;</button>
+              <button style={{padding: '1em'}} className="pure-button" onClick={this.stop}>&#9632;</button>
+              <label>Prometheus URL
+                <input type="text" placeholder="Prometheus URL" value={this.state.url} onInput={linkState(this, 'url')}/>
+              </label>
+              <label>End date
+                <input type="text" placeholder="End date" value={this.state.end} onInput={linkState(this, 'end')}/>
+              </label>
+              <label>Range (seconds)
+                <input type="text" placeholder="Range (seconds)" value={this.state.range} onInput={linkState(this, 'range')}/>
+              </label>
+              <label>Number of samples
+                <input type="text" placeholder="Number of samples" value={this.state.sampleCount} onInput={linkState(this, 'sampleCount')}/>
+              </label>
+              <label>Sample Duration
+                <input type="text" placeholder="Sample duration" value={this.state.sampleDuration} onInput={linkState(this, 'sampleDuration')}/>
+              </label>
+            </fieldset>
+          </div>
+        </div>
       </form>
     </div>;
   }
